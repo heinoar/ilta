b1f0381d724056d4e3a25274f608f463e230bcc0
ari_heinonen <ari_heinonen@hotmail.com>
2022-04-09 19:57:38 +0300

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-'''
+
 #import urllib3
 from flask import Flask
 
@@ -10,21 +10,4 @@ def index():
     return 'Web App with Python Flask!'
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=8000)
-'''
-@app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
+  app.run()
