2646022d9624f0e820f6f67a46f60bd37ae37436
ari_heinonen <ari_heinonen@hotmail.com>
2022-04-10 21:00:26 +0300

    Revert to old

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    conn_str=os.environ.get('MYSQLCONNSTR_PRIMARY_CONNECTION_STRING','ei löytynny')
-    return conn_str
-    #return 'Web App with Python Flask!'
+    #conn_str=os.environ.get('MYSQLCONNSTR_PRIMARY_CONNECTION_STRING','ei löytynny')
+    #return conn_str
+    return 'Web App with Python Flask!'
 
 if __name__ == "__main__":
   app.run()
