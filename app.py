10d64f3d02e53e441d63f75f5ac035fc8cfc017b
ari_heinonen <ari_heinonen@hotmail.com>
2022-04-10 20:57:12 +0300

    Conn string test

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 
-#import urllib3
 from flask import Flask
+import os
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Web App with Python Flask!'
+    conn_str=os.environ.get('MYSQLCONNSTR_PRIMARY_CONNECTION_STRING','ei löytynny')
+    return conn_str
+    #return 'Web App with Python Flask!'
 
 if __name__ == "__main__":
   app.run()
